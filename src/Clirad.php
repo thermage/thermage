080e4d3b26112d1e992769c368ab820c18381cb1
Awilum <sergey.romanenko@flextype.org>
2021-10-01 10:45:59 +0300

    feat(cliprad): rename `ConsoleOutput` to `SymfonyRenderer`

--- src/Clirad.php
+++ src/Clirad.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Clirad;
 
 use Clirad\Components\Element;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleOutput as SymfonyRenderer;
 
 use function array_map;
 use function is_array;
@@ -22,7 +22,7 @@ final class Clirad
     public static function element(string $value = '', array $properties = []): Element
     {
         return new Element(
-            self::$renderer ?? new ConsoleOutput(),
+            self::$renderer ?? new SymfonyRenderer(),
             $value,
             $properties
         );
