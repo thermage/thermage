42560398d9791e78426315c8397aeae968919069
Awilum <sergey.romanenko@flextype.org>
2021-10-01 10:59:44 +0300

    feat(helpers): finetune helpers

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,18 @@
 declare(strict_types=1);
 
 use Clirad\Clirad;
+use Clirad\Components\Element;
 
-function el(string $value = '', array $properties = []) {
-    return Clirad::element($value, $properties);
+if (! function_exists('el')) {
+    /**
+     * Create Element Component.
+     * 
+     * @param string $value      Element value.
+     * @param array  $properties Element properties.
+     * 
+     * @return Element Element Component.
+     */
+    function el(string $value = '', array $properties = []): Element {
+        return Clirad::element($value, $properties);
+    }
 }
