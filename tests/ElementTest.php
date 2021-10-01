9c91ac407d0accc59391a00aae1554e3e9b3c0f9
Awilum <sergey.romanenko@flextype.org>
2021-10-01 16:49:00 +0300

    feat(tests): add tests for margins and paddings

--- tests/ElementTest.php
+++ tests/ElementTest.php
@@ -65,3 +65,63 @@ test('test properties and getProperties', function (): void {
     $value = el()->properties(['RAD']);
     expect($value->getProperties()->toArray())->toBe(['RAD']); 
 });
+
+test('test mx', function (): void {
+    $value = el('RAD')->mx(10)->render();
+    expect($value)->toBe('     RAD     '); 
+});
+
+test('test magic mx', function (): void {
+    $value = el('RAD')->mx10()->render();
+    expect($value)->toBe('     RAD     '); 
+});
+
+test('test mr', function (): void {
+    $value = el('RAD')->mr(10)->render();
+    expect($value)->toBe('RAD          '); 
+});
+
+test('test magic mr', function (): void {
+    $value = el('RAD')->mr10()->render();
+    expect($value)->toBe('RAD          '); 
+});
+
+test('test ml', function (): void {
+    $value = el('RAD')->ml(10)->render();
+    expect($value)->toBe('          RAD'); 
+});
+
+test('test magic ml', function (): void {
+    $value = el('RAD')->ml10()->render();
+    expect($value)->toBe('          RAD'); 
+});
+
+test('test px', function (): void {
+    $value = el('RAD')->px(10)->render();
+    expect($value)->toBe('     RAD     '); 
+});
+
+test('test magic px', function (): void {
+    $value = el('RAD')->px10()->render();
+    expect($value)->toBe('     RAD     '); 
+});
+
+test('test pr', function (): void {
+    $value = el('RAD')->pr(10)->render();
+    expect($value)->toBe('RAD          '); 
+});
+
+test('test magic pr', function (): void {
+    $value = el('RAD')->pr10()->render();
+    expect($value)->toBe('RAD          '); 
+});
+
+test('test pl', function (): void {
+    $value = el('RAD')->pl(10)->render();
+    expect($value)->toBe('          RAD'); 
+});
+
+test('test magic pl', function (): void {
+    $value = el('RAD')->pl10()->render();
+    expect($value)->toBe('          RAD'); 
+});
