a6ff5ff4f801400902fe92693b4eccfbf8208734
Awilum <sergey.romanenko@flextype.org>
2021-10-07 10:36:52 +0300

    refactor(link): finetune `Link` class

--- src/Components/Link.php
+++ src/Components/Link.php
@@ -12,7 +12,7 @@ final class Link extends Element
      * Set link href property.
      *
      * @param string $value Href value.
-     * 
+     *
      * @return self Returns instance of the Link class.
      *
      * @access public
