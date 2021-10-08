48b2b1feb35152d44c6445ea1417673aa18ffd89
Awilum <sergey.romanenko@flextype.org>
2021-10-08 21:26:47 +0300

    feat(shortcodes): add Shortcodes basic class

---
+++ src/Parsers/Shortcodes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Termage\Parsers;
+
+use Thunder\Shortcode\ShortcodeFacade;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class Shortcodes
+{
+
+}
