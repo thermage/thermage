2f3077b1fbfec2a517903eb97cf11d8429d43775
Awilum <sergey.romanenko@flextype.org>
2021-10-11 15:58:40 +0300

    fix(tests): fix tests for el component

--- tests/ElTest.php
+++ tests/ElTest.php
@@ -14,62 +14,62 @@ test('test termage el method', function (): void {
 
 test('test render', function (): void {
     $value = termage()->el()->value('RAD')->render();
-    expect($value)->toEqual('RAD');
+    expect($value)->toEqual('[m l=0 r=0][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test color', function (): void {
     $value = termage()->el()->value('RAD')->color('blue')->render();
-    expect($value)->toBe('<fg=#007bff;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][color=blue][p l=0 r=0]RAD[/p][/color][/m]');
 });
 
 test('test magic color', function (): void {
     $value = termage()->el()->value('RAD')->colorBlue()->render();
-    expect($value)->toBe('<fg=#007bff;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][color=blue][p l=0 r=0]RAD[/p][/color][/m]');
 });
 
 test('test bg', function (): void {
     $value = termage()->el()->value('RAD')->bg('blue')->render();
-    expect($value)->toBe('<bg=#007bff;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][bg=blue][p l=0 r=0]RAD[/p][/bg][/m]');
 });
 
 test('test magic bg', function (): void {
     $value = termage()->el()->value('RAD')->bgBlue()->render();
-    expect($value)->toBe('<bg=#007bff;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][bg=blue][p l=0 r=0]RAD[/p][/bg][/m]');
 });
 
 test('test bold', function (): void {
     $value = termage()->el()->value('RAD')->bold()->render();
-    expect($value)->toBe('<options=bold;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][b]RAD[/b][/p][/m]');
 });
 
 test('test underscore', function (): void {
     $value = termage()->el()->value('RAD')->underscore()->render();
-    expect($value)->toBe('<options=underscore;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][u]RAD[/u][/p][/m]');
 });
 
 test('test underline', function (): void {
     $value = termage()->el()->value('RAD')->underline()->render();
-    expect($value)->toBe('<options=underscore;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][u]RAD[/u][/p][/m]');
 });
 
 test('test blink', function (): void {
     $value = termage()->el()->value('RAD')->blink()->render();
-    expect($value)->toBe('<options=blink;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][blink]RAD[/blink][/p][/m]');
 });
 
 test('test reverse', function (): void {
     $value = termage()->el()->value('RAD')->reverse()->render();
-    expect($value)->toBe('<options=reverse;>RAD</>');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][reverse]RAD[/reverse][/p][/m]');
 });
 
-test('test conceal', function (): void {
-    $value = termage()->el()->value('RAD')->conceal()->render();
-    expect($value)->toBe('<options=conceal;>RAD</>');
+test('test invisible', function (): void {
+    $value = termage()->el()->value('RAD')->invisible()->render();
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=0][invisible]RAD[/invisible][/p][/m]');
 });
 
 test('test value and getValue', function (): void {
     $value = termage()->el()->value('RAD');
-    expect($value->render())->toBe('RAD');
+    expect($value->render())->toBe('[m l=0 r=0][p l=0 r=0]RAD[/p][/m]');
     expect($value->getValue()->toString())->toBe('RAD');
 });
 
@@ -80,82 +80,62 @@ test('test properties and getProperties', function (): void {
 
 test('test mx', function (): void {
     $value = termage()->el()->value('RAD')->mx(10)->render();
-    expect($value)->toBe('     RAD     ');
+    expect($value)->toBe('[m l=5 r=5][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test magic mx', function (): void {
     $value = termage()->el()->value('RAD')->mx10()->render();
-    expect($value)->toBe('     RAD     ');
+    expect($value)->toBe('[m l=5 r=5][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test mr', function (): void {
     $value = termage()->el()->value('RAD')->mr(10)->render();
-    expect($value)->toBe('RAD          ');
+    expect($value)->toBe('[m l=0 r=10][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test magic mr', function (): void {
     $value = termage()->el()->value('RAD')->mr10()->render();
-    expect($value)->toBe('RAD          ');
+    expect($value)->toBe('[m l=0 r=10][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test ml', function (): void {
     $value = termage()->el()->value('RAD')->ml(10)->render();
-    expect($value)->toBe('          RAD');
+    expect($value)->toBe('[m l=10 r=0][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test magic ml', function (): void {
     $value = termage()->el()->value('RAD')->ml10()->render();
-    expect($value)->toBe('          RAD');
+    expect($value)->toBe('[m l=10 r=0][p l=0 r=0]RAD[/p][/m]');
 });
 
 test('test px', function (): void {
     $value = termage()->el()->value('RAD')->px(10)->render();
-    expect($value)->toBe('     RAD     ');
+    expect($value)->toBe('[m l=0 r=0][p l=5 r=5]RAD[/p][/m]');
 });
 
 test('test magic px', function (): void {
     $value = termage()->el()->value('RAD')->px10()->render();
-    expect($value)->toBe('     RAD     ');
+    expect($value)->toBe('[m l=0 r=0][p l=5 r=5]RAD[/p][/m]');
 });
 
 test('test pr', function (): void {
     $value = termage()->el()->value('RAD')->pr(10)->render();
-    expect($value)->toBe('RAD          ');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=10]RAD[/p][/m]');
 });
 
 test('test magic pr', function (): void {
     $value = termage()->el()->value('RAD')->pr10()->render();
-    expect($value)->toBe('RAD          ');
+    expect($value)->toBe('[m l=0 r=0][p l=0 r=10]RAD[/p][/m]');
 });
 
 test('test pl', function (): void {
     $value = termage()->el()->value('RAD')->pl(10)->render();
-    expect($value)->toBe('          RAD');
+    expect($value)->toBe('[m l=0 r=0][p l=10 r=0]RAD[/p][/m]');
 });
 
 test('test magic pl', function (): void {
     $value = termage()->el()->value('RAD')->pl10()->render();
-    expect($value)->toBe('          RAD');
-});
-
-test('test lower', function (): void {
-    $value = termage()->el()->value('RAD')->lower()->render();
-    expect($value)->toBe('rad');
-});
-
-test('test upper', function (): void {
-    $value = termage()->el()->value('rad')->upper()->render();
-    expect($value)->toBe('RAD');
-});
-
-test('test camel', function (): void {
-    $value = termage()->el()->value('RaD')->camel()->render();
-    expect($value)->toBe('raD');
-});
-
-test('test capitalize', function (): void {
-    $value = termage()->el()->value('RAD')->capitalize()->render();
-    expect($value)->toBe('Rad');
+    expect($value)->toBe('[m l=0 r=0][p l=10 r=0]RAD[/p][/m]');
 });
 
 test('test getOutput', function (): void {
@@ -179,21 +159,9 @@ test('magic throw exception BadMethodCallException', function (): void {
 
 test('test magic __toString', function (): void {
     $value = termage()->el()->value('RAD');
-    expect((string) $value)->toBe('RAD');
+    expect((string) $value)->toBe('[m l=0 r=0][p l=0 r=0]RAD[/p][/m]');
 });
 
-test('test limit', function (): void {
-    $value = termage()->el()->value('RAD')->limit(1)->render();
-    expect($value)->toBe('R...');
-
-    $value = termage()->el()->value('RAD')->limit(1, '')->render();
-    expect($value)->toBe('R');
-});
-
-test('test repeat', function (): void {
-    $value = termage()->el()->value('RAD')->repeat(3)->render();
-    expect($value)->toBe('RADRADRAD');
-});
 
 test('test display', function (): void {
     $output = new BufferedOutput();
