20dde73d341f1c8697245b42138613dfa4b1acf2
Awilum <sergey.romanenko@flextype.org>
2021-10-11 22:54:48 +0300

    feat(tests): improve tests for `Shortcodes`

--- tests/ShortcodesTest.php
+++ tests/ShortcodesTest.php
@@ -20,7 +20,28 @@ test('test addHandler and parse and parseText method', function (): void {
     $shortcodes->addHandler('foo', function($s){ 
         return 'Foo';
     });
-    
+
     expect($shortcodes->parseText('[foo]'))->toBeArray();
     expect($shortcodes->parse('[foo]'))->toEqual('Foo');
+});
+
+test('test stripShortcodes method', function (): void {
+    $shortcodes = termage()->getShortcodes();
+
+    expect($shortcodes->stripShortcodes('[foo]Foo[/foo]'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo=bar]Foo[/foo]'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo="bar"]Foo[/foo]'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo x="1" y="2"]Foo[/foo]'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo]Foo[/foo] Foo'))->toEqual('Foo Foo');
+    expect($shortcodes->stripShortcodes('[foo]Foo[/foo] Foo [foo]Foo[/foo]'))->toEqual('Foo Foo Foo');
+    expect($shortcodes->stripShortcodes('[foo/]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo /]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo="Foo" /]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo ]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo x="1" y="2"]Foo'))->toEqual('Foo');
+    expect($shortcodes->stripShortcodes('[foo]F[o]o[/foo]'))->toEqual('Fo');
+
+    // @todo: find a way to process these cases:
+    // expect($shortcodes->stripShortcodes('[foo]F[oo[/foo]'))->toEqual('F[oo');
 });
