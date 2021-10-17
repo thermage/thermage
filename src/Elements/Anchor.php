8c6ee88d90fd0f341cd73094343e93d9538cb1d7
Awilum <sergey.romanenko@flextype.org>
2021-10-17 22:06:12 +0300

    feat(elements): add Anchor element

--- src/Elements/Anchor.php
+++ src/Elements/Anchor.php
@@ -2,24 +2,24 @@
 
 declare(strict_types=1);
 
-namespace Termage\Components;
+namespace Termage\Elements;
 
 use Termage\Base\Element;
 
-final class Link extends Element
+final class Anchor extends Element
 {
     /**
      * Set link href property.
      *
      * @param string $value Href value.
      *
-     * @return self Returns instance of the Link class.
+     * @return self Returns instance of the Anchor class.
      *
      * @access public
      */
     public function href(string $value): self
     {
-        $this->value('[link href=' . $value . ']' . (string) $this->getValue() . '[/link]');
+        $this->setValue("\e]8;;" . $value . "\e\\" . $this->getValue() . "\e]8;;\e\\");
 
         return $this;
     }
