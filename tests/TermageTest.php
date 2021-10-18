d3d197b54a4ec767908dfd147f7ad88ffbdc04e1
Awilum <sergey.romanenko@flextype.org>
2021-10-18 17:27:37 +0300

    feat(tests): update tests for `Termage`

--- tests/TermageTest.php
+++ tests/TermageTest.php
@@ -3,82 +3,80 @@
 declare(strict_types=1);
 
 use Termage\Termage;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Terminal;
-use Termage\Components\Alert;
-use Termage\Components\El;
-use Termage\Components\Emoji;
-use Termage\Components\Link;
-use Termage\Components\Rule;
-use Termage\Components\Chart;
+use Termage\Elements\Alert;
+use Termage\Elements\Div;
+use Termage\Elements\Anchor;
+use Termage\Elements\Blink;
+use Termage\Elements\Bold;
+use Termage\Elements\Breakline;
+use Termage\Elements\Chart;
+use Termage\Elements\Hr;
+use Termage\Elements\Invisible;
+use Termage\Elements\Italic;
+use Termage\Elements\Paragraph;
+use Termage\Elements\Reverse;
+use Termage\Elements\Span;
+use Termage\Elements\Strikethrough;
+use Termage\Elements\Underline;
 use Termage\Base\Theme;
 use Termage\Parsers\Shortcodes;
 
-test('test set and get output', function (): void {
-    $termage = termage();
-    $this->assertInstanceOf(ConsoleOutput::class, $termage->getOutput());
-
-    $termage = termage(new ConsoleOutput());
-    $this->assertInstanceOf(ConsoleOutput::class, $termage->getOutput());
-
-    $termage = termage(new BufferedOutput());
-    $this->assertInstanceOf(BufferedOutput::class, $termage->getOutput());
-
-    $termage = termage()->output(new BufferedOutput());
-    $this->assertInstanceOf(BufferedOutput::class, $termage->getOutput());
-});
-
-test('test getTerminal', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Terminal::class, $termage->getTerminal());
-});
-
 test('test getTheme', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Theme::class, $termage->getTheme());
+    $this->assertInstanceOf(Theme::class, Termage::getTheme());
 });
 
 test('test alert', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Alert::class, $termage->alert());
+    $this->assertInstanceOf(Alert::class, Termage::alert());
 });
 
-test('test el', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(El::class, $termage->el());
+test('test anchor', function (): void {
+    $this->assertInstanceOf(Anchor::class, Termage::anchor());
 });
 
-test('test rule', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Rule::class, $termage->rule());
+test('test blink', function (): void {
+    $this->assertInstanceOf(Blink::class, Termage::blink());
 });
 
-test('test emoji', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Emoji::class, $termage->emoji());
+test('test bold', function (): void {
+    $this->assertInstanceOf(Bold::class, Termage::bold());
 });
 
-test('test link', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Link::class, $termage->link());
+test('test breakline', function (): void {
+    $this->assertInstanceOf(Breakline::class, Termage::breakline());
 });
 
 test('test chart', function (): void {
-    $termage = termage();
+    $this->assertInstanceOf(Chart::class, Termage::chart());
+});
 
-    $this->assertInstanceOf(Chart::class, $termage->chart());
+test('test invisible', function (): void {
+    $this->assertInstanceOf(Invisible::class, Termage::invisible());
+});
+
+test('test italic', function (): void {
+    $this->assertInstanceOf(Italic::class, Termage::italic());
+});
+
+test('test paragraph', function (): void {
+    $this->assertInstanceOf(Paragraph::class, Termage::paragraph());
+});
+
+test('test reverse', function (): void {
+    $this->assertInstanceOf(Reverse::class, Termage::reverse());
+});
+
+test('test span', function (): void {
+    $this->assertInstanceOf(Span::class, Termage::span());
+});
+
+test('test strikethrough', function (): void {
+    $this->assertInstanceOf(Strikethrough::class, Termage::strikethrough());
+});
+
+test('test underline', function (): void {
+    $this->assertInstanceOf(Underline::class, Termage::underline());
 });
 
 test('test getShortcodes', function (): void {
-    $termage = termage();
-
-    $this->assertInstanceOf(Shortcodes::class, $termage->getShortcodes());
+    $this->assertInstanceOf(Shortcodes::class, Termage::getShortcodes());
 });
