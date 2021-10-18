caaaeb77ec958db1998560ace0bebff869af8d2c
Awilum <sergey.romanenko@flextype.org>
2021-10-18 11:41:27 +0300

    refactor(core): update headers

--- src/Elements/Hr.php
+++ src/Elements/Hr.php
@@ -10,7 +10,10 @@ declare(strict_types=1);
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
  *
+ * @author    Sergey Romanenko <sergey.romanenko@flextype.org>
+ * @copyright Copyright (c) Sergey Romanenko (https://awilum.github.io)
  * @link      https://digital.flextype.org/termage/ Termage
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace Termage\Elements;
