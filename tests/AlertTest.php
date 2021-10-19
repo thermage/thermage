bd9e171486fb2ab0545e288b9e28f601802a4b9b
Awilum <sergey.romanenko@flextype.org>
2021-10-19 12:41:03 +0300

    fix(tests): fix test on Github

--- tests/AlertTest.php
+++ tests/AlertTest.php
@@ -15,55 +15,55 @@ beforeEach(function() {
 
 test('test alert info', function (): void {
     $value = alert('Stay RAD!')->info()->render();
-    $alert = "\e[48;2;0;123;255m\e[49m\e[38;2;0;0;0m\e[48;2;0;123;255mStayRAD!\e[49m\e[39m\e[48;2;0;123;255m\e[49m";
+    $alert = "\e[44m\e[49m\e[30m\e[44mStayRAD!\e[49m\e[39m\e[44m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert warning', function (): void {
     $value = alert('Stay RAD!')->warning()->render();
-    $alert = "\e[48;2;255;193;7m\e[49m\e[38;2;0;0;0m\e[48;2;255;193;7mStayRAD!\e[49m\e[39m\e[48;2;255;193;7m\e[49m";
+    $alert = "\e[43m\e[49m\e[30m\e[43mStayRAD!\e[49m\e[39m\e[43m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert danger', function (): void {
     $value = alert('Stay RAD!')->danger()->render();
-    $alert = "\e[48;2;220;53;69m\e[49m\e[38;2;255;255;255m\e[48;2;220;53;69mStayRAD!\e[49m\e[39m\e[48;2;220;53;69m\e[49m";
+    $alert = "\e[41m\e[49m\e[37m\e[41mStayRAD!\e[49m\e[39m\e[41m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert success', function (): void {
     $value = alert('Stay RAD!')->success()->render();
-    $alert = "\e[48;2;40;167;69m\e[49m\e[38;2;0;0;0m\e[48;2;40;167;69mStayRAD!\e[49m\e[39m\e[48;2;40;167;69m\e[49m";
+    $alert = "\e[42m\e[49m\e[30m\e[42mStayRAD!\e[49m\e[39m\e[42m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert primary', function (): void {
     $value = alert('Stay RAD!')->primary()->render();
-    $alert = "\e[48;2;0;123;255m\e[49m\e[38;2;255;255;255m\e[48;2;0;123;255mStayRAD!\e[49m\e[39m\e[48;2;0;123;255m\e[49m";
+    $alert = "\e[44m\e[49m\e[37m\e[44mStayRAD!\e[49m\e[39m\e[44m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert secondary', function (): void {
     $value = alert('Stay RAD!')->secondary()->render();
-    $alert = "\e[100m\e[49m\e[38;2;0;0;0m\e[100mStayRAD!\e[49m\e[39m\e[100m\e[49m";
+    $alert = "\e[100m\e[49m\e[30m\e[100mStayRAD!\e[49m\e[39m\e[100m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert with text align right', function (): void {
     $value = alert('Stay RAD!')->textAlignRight()->render();
-    $alert = "\e[48;2;0;123;255m\e[49m\e[38;2;0;0;0m\e[48;2;0;123;255mStayRAD!\e[49m\e[39m\e[48;2;0;123;255m\e[49m";
+    $alert = "\e[44m\e[49m\e[30m\e[44mStayRAD!\e[49m\e[39m\e[44m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert with text align left', function (): void {
     $value = alert('Stay RAD!')->textAlignLeft()->render();
-    $alert = "\e[48;2;0;123;255m\e[49m\e[38;2;0;0;0m\e[48;2;0;123;255mStayRAD!\e[49m\e[39m\e[48;2;0;123;255m\e[49m";
+    $alert = "\e[44m\e[49m\e[30m\e[44mStayRAD!\e[49m\e[39m\e[44m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
 test('test alert size', function (): void {
     $value = alert('Stay RAD!')->size(200)->render();
-    $alert = "\e[48;2;0;123;255m\e[49m\e[38;2;0;0;0m\e[48;2;0;123;255mStayRAD!\e[49m\e[39m\e[48;2;0;123;255m\e[49m";
+    $alert = "\e[44m\e[49m\e[30m\e[44mStayRAD!\e[49m\e[39m\e[44m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($alert);
 });
 
--- tests/ChartTest.php
+++ tests/ChartTest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 use Termage\Termage;
 use Termage\Themes\Theme;
+use Termage\Themes\ThemeInterface;
 use Termage\Elements\Chart;
 use function Termage\chart;
+use function Termage\setTheme;
 
 beforeEach(function() {
-   $this->data = [
+    setTheme(new ChartTestTheme());
+
+    $this->data = [
         'apple' => [
             'label' => 'Apple',
             'value' => 100,
@@ -17,7 +21,7 @@ beforeEach(function() {
         'orange' => [
             'label' => 'Orange',
             'value' => 270,
-            'color' => 'orange',
+            'color' => 'yellow',
         ],
         'lime' => [
             'label' => 'Lime',
@@ -29,12 +33,30 @@ beforeEach(function() {
 
 test('test chart horizontal', function (): void {
     $value = chart()->setData($this->data)->horizontal()->render();
-    $chart = "\e[38;2;220;53;69mApple\e[39m\e[48;2;220;53;69m\e[49m\e[38;2;253;126;20mOrange\e[39m\e[48;2;253;126;20m\e[49m\e[38;2;40;167;69mLime\e[39m\e[48;2;40;167;69m\e[49m";
+    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[33mOrange\e[39m\e[43m\e[49m\e[32mLime\e[39m\e[42m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
 });
 
 test('test chart inline', function (): void {
     $value = chart()->setData($this->data)->horizontal()->render();
-    $chart = "\e[38;2;220;53;69mApple\e[39m\e[48;2;220;53;69m\e[49m\e[38;2;253;126;20mOrange\e[39m\e[48;2;253;126;20m\e[49m\e[38;2;40;167;69mLime\e[39m\e[48;2;40;167;69m\e[49m";
+    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[33mOrange\e[39m\e[43m\e[49m\e[32mLime\e[39m\e[42m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
 });
+
+class ChartTestTheme extends Theme implements ThemeInterface
+{
+    public function getThemeVariables(): array
+    {
+        return [
+            'colors' => [
+                'blue' => 'blue',
+                'yellow' => 'yellow',
+                'black' => 'black',
+                'white' => 'white',
+                'red' => 'red',
+                'green' => 'green',
+                'gray' => 'gray',
+            ],
+        ];
+    }
+}
