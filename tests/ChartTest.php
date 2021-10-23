2a6eceb0d29356752bed75f0e68db8e1e70721f4
Awilum <sergey.romanenko@flextype.org>
2021-10-23 21:47:20 +0300

    feat(tests): add more tests

--- tests/ChartTest.php
+++ tests/ChartTest.php
@@ -31,15 +31,33 @@ beforeEach(function() {
     ];
 });
 
-test('test chart horizontal', function (): void {
+test('test horizontal', function (): void {
     $value = chart()->data($this->data)->horizontal()->render();
     $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[33mOrange\e[39m\e[43m\e[49m\e[32mLime\e[39m\e[42m\e[49m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
 });
 
-test('test chart inline', function (): void {
-    $value = chart()->data($this->data)->horizontal()->render();
-    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[33mOrange\e[39m\e[43m\e[49m\e[32mLime\e[39m\e[42m\e[49m";
+test('test inline', function (): void {
+    $value = chart()->data($this->data)->inline()->render();
+    $chart = "\e[41m\e[49m\e[43m\e[49m\e[42m\e[49m\e[31mApple\e[39m\e[33mOrange\e[39m\e[32mLime\e[39m";
+    expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
+});
+
+test('test showPercents', function (): void {
+    $value = chart()->data($this->data)->showPercents()->render();
+    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[31m17%\e[39m\e[33mOrange\e[39m\e[43m\e[49m\e[33m46%\e[39m\e[32mLime\e[39m\e[42m\e[49m\e[32m37%\e[39m";
+    expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
+});
+
+test('test showValues', function (): void {
+    $value = chart()->data($this->data)->showValues()->render();
+    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[31m(100)\e[39m\e[33mOrange\e[39m\e[43m\e[49m\e[33m(270)\e[39m\e[32mLime\e[39m\e[42m\e[49m\e[32m(220)\e[39m";
+    expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
+});
+
+test('test valuesSufix', function (): void {
+    $value = chart()->data($this->data)->showValues()->valuesSufix('items')->render();
+    $chart = "\e[31mApple\e[39m\e[41m\e[49m\e[31m(100items)\e[39m\e[33mOrange\e[39m\e[43m\e[49m\e[33m(270items)\e[39m\e[32mLime\e[39m\e[42m\e[49m\e[32m(220items)\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($chart);
 });
 
