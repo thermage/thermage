9ca12d08440f53e0349e8fe0577d65784531e133
Awilum <sergey.romanenko@flextype.org>
2021-10-24 15:27:22 +0300

    feat(tests): update tests

--- tests/HrTest.php
+++ tests/HrTest.php
@@ -15,63 +15,63 @@ beforeEach(function() {
 test('test hr', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->render();
-    $hr = "\e[38;2;23;162;184m―――\e[39m\e[38;2;23;162;184mStayRAD!\e[39m\e[38;2;23;162;184m――――――\e[39m";
+    $hr = "\e[34m―――\e[39m\e[34mStayRAD!\e[39m\e[34m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test hr with text align left', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->textAlignLeft()->render();
-    $hr = "\e[38;2;23;162;184m―――\e[39m\e[38;2;23;162;184mStayRAD!\e[39m\e[38;2;23;162;184m――――――\e[39m";
+    $hr = "\e[34m―――\e[39m\e[34mStayRAD!\e[39m\e[34m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test hr with text align right', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->textAlignRight()->render();
-    $hr = "\e[38;2;23;162;184m――――――\e[39m\e[38;2;23;162;184mStayRAD!\e[39m\e[38;2;23;162;184m―――\e[39m";
+    $hr = "\e[34m――――――\e[39m\e[34mStayRAD!\e[39m\e[34m―――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test hr info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->info()->render();
-    $hr = "\e[38;2;23;162;184m―――\e[39m\e[38;2;23;162;184mStayRAD!\e[39m\e[38;2;23;162;184m――――――\e[39m";
+    $hr = "\e[34m―――\e[39m\e[34mStayRAD!\e[39m\e[34m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test danger info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->danger()->render();
-    $hr = "\e[38;2;220;53;69m―――\e[39m\e[38;2;220;53;69mStayRAD!\e[39m\e[38;2;220;53;69m――――――\e[39m";
+    $hr = "\e[31m―――\e[39m\e[31mStayRAD!\e[39m\e[31m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test warning info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->warning()->render();
-    $hr = "\e[38;2;255;193;7m―――\e[39m\e[38;2;255;193;7mStayRAD!\e[39m\e[38;2;255;193;7m――――――\e[39m";
+    $hr = "\e[33m―――\e[39m\e[33mStayRAD!\e[39m\e[33m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test success info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->success()->render();
-    $hr = "\e[38;2;40;167;69m―――\e[39m\e[38;2;40;167;69mStayRAD!\e[39m\e[38;2;40;167;69m――――――\e[39m";
+    $hr = "\e[32m―――\e[39m\e[32mStayRAD!\e[39m\e[32m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test primary info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->success()->render();
-    $hr = "\e[38;2;40;167;69m―――\e[39m\e[38;2;40;167;69mStayRAD!\e[39m\e[38;2;40;167;69m――――――\e[39m";
+    $hr = "\e[32m―――\e[39m\e[32mStayRAD!\e[39m\e[32m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
 test('test secondary info', function (): void {
     putenv('COLUMNS=20');
     $value = hr('Stay RAD!')->secondary()->render();
-    $hr = "\e[38;2;108;117;125m―――\e[39m\e[38;2;108;117;125mStayRAD!\e[39m\e[38;2;108;117;125m――――――\e[39m";
+    $hr = "\e[90m―――\e[39m\e[90mStayRAD!\e[39m\e[90m――――――\e[39m";
     expect(str_replace(["\r\n", "\r", "\n", " "], "", strings($value)->trim()->toString()))->toEqual($hr);
 });
 
@@ -93,12 +93,12 @@ class HrTestTheme extends Theme implements ThemeInterface
             'hr' => [
                 'text-align' => 'left',
                 'type' => [
-                    'info' => ['color' => 'info'],
-                    'warning' => ['color' => 'warning'],
-                    'danger' => ['color' => 'danger'],
-                    'success' => ['color' => 'success'],
-                    'primary' => ['color' => 'primary'],
-                    'secondary' => ['color' => 'secondary'],
+                    'info' => ['color' => 'blue'],
+                    'warning' => ['color' => 'yellow'],
+                    'danger' => ['color' => 'red'],
+                    'success' => ['color' => 'green'],
+                    'primary' => ['color' => 'blue'],
+                    'secondary' => ['color' => 'gray'],
                 ],
             ],
         ];
