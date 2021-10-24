25511aff6504bb2e79752a5f61348625f557e301
Awilum <sergey.romanenko@flextype.org>
2021-10-24 23:59:58 +0300

    feat(tests): update tests for Shortcodes

--- tests/ShortcodesTest.php
+++ tests/ShortcodesTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Termage\Termage;
 use Termage\Parsers\Shortcodes;
 use Thunder\Shortcode\ShortcodeFacade;
+use Thunder\Shortcode\EventHandler\FilterRawEventHandler;
+use Thunder\Shortcode\Events;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 use Termage\Themes\Theme;
 use Termage\Themes\ThemeInterface;
 
@@ -33,6 +36,14 @@ test('test add and parse and parseText method', function (): void {
     expect(Termage::getShortcodes()->parse('[foo]'))->toEqual('Foo');
 });
 
+test('test addEventHandler', function (): void {
+    Termage::getShortcodes()->add('raw', static function (ShortcodeInterface $s) {
+        return $s->getContent();
+    });
+    Termage::getShortcodes()->addEvent(Events::FILTER_SHORTCODES, new FilterRawEventHandler(['raw']));
+    expect(Termage::getShortcodes()->parse('[raw][foo][/raw]'))->toEqual('[foo]');
+});
+
 test('test stripShortcodes method', function (): void {
     expect(Termage::getShortcodes()->stripShortcodes('[foo]Foo[/foo]'))->toEqual('Foo');
     expect(Termage::getShortcodes()->stripShortcodes('[foo=bar]Foo[/foo]'))->toEqual('Foo');
@@ -52,92 +63,86 @@ test('test stripShortcodes method', function (): void {
     // expect(Termage::getShortcodes()->stripShortcodes('[foo]F[oo[/foo]'))->toEqual('F[oo');
 });
 
-test('test [bold] and [b] shortcodes', function (): void {
-    
+test('test [bold] and [b] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[bold]bold[/bold]'))->toEqual("\e[1mbold\e[22m");
     expect(Termage::getShortcodes()->parse('[b]bold[/b]'))->toEqual("\e[1mbold\e[22m");
 });
 
-test('test [italic] and [i] shortcodes', function (): void {
-    
+test('test [italic] and [i] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[italic]italic[/italic]'))->toEqual("\e[3mitalic\e[23m");
     expect(Termage::getShortcodes()->parse('[i]italic[/i]'))->toEqual("\e[3mitalic\e[23m");
 });
 
-test('test [underline] and [u] shortcodes', function (): void {
-    
+test('test [underline] and [u] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[underline]underline[/underline]'))->toEqual("\e[4munderline\e[24m");
     expect(Termage::getShortcodes()->parse('[u]underline[/u]'))->toEqual("\e[4munderline\e[24m");
 });
 
-test('test [strikethrough] and [s] shortcodes', function (): void {
+test('test [strikethrough] and [s] shortcode', function (): void {
     
     expect(Termage::getShortcodes()->parse('[strikethrough]strikethrough[/strikethrough]'))->toEqual("\e[9mstrikethrough\e[29m");
     expect(Termage::getShortcodes()->parse('[s]strikethrough[/s]'))->toEqual("\e[9mstrikethrough\e[29m");
 });
 
-test('test [dim] and [d] shortcodes', function (): void {
-    
+test('test [dim] and [d] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[dim]dim[/dim]'))->toEqual("\e[2mdim\e[22m");
     expect(Termage::getShortcodes()->parse('[d]dim[/d]'))->toEqual("\e[2mdim\e[22m");
 });
 
-test('test [blink] shortcodes', function (): void {
-    
+test('test [blink] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[blink]blink[/blink]'))->toEqual("\e[5mblink\e[25m");
 });
 
-test('test [reverse] shortcodes', function (): void {
-    
+test('test [reverse] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[reverse]reverse[/reverse]'))->toEqual("\e[7mreverse\e[27m");
 });
 
-test('test [invisible] shortcodes', function (): void {
-    
+test('test [invisible] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[invisible]invisible[/invisible]'))->toEqual("\e[8minvisible\e[28m");
 });
 
-test('test [anchor] shortcodes', function (): void {
-    
+test('test [anchor] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[anchor href="https://github.com/termage/termage"]Termage[/anchor]'))->toEqual("\e]8;;https://github.com/termage/termage\e\\Termage\e]8;;\e\\");
 });
 
-test('test [m l= r=] shortcodes', function (): void {
-    
+test('test [m l= r=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[m l=2 r=2]Margin left and right[/m]'))->toEqual("  Margin left and right  ");
 });
 
-test('test [mx=] shortcodes', function (): void {
-    
+test('test [mx=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[mx=4]Margin left and right[/mx]'))->toEqual("  Margin left and right  ");
 });
 
-test('test [ml=] shortcodes', function (): void {
-    
+test('test [ml=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[ml=2]Margin left[/ml]'))->toEqual("  Margin left");
 });
 
-test('test [mr=] shortcodes', function (): void {
-    
+test('test [mr=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[mr=2]Margin right[/mr]'))->toEqual("Margin right  ");
 });
 
-test('test [p l= r=] shortcodes', function (): void {
-    
+test('test [p l= r=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[p l=2 r=2]Padding left and right[/p]'))->toEqual("  Padding left and right  ");
 });
 
-test('test [px=] shortcodes', function (): void {
+test('test [px=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[px=4]Padding left and right[/px]'))->toEqual("  Padding left and right  ");
 });
 
-test('test [pl=] shortcodes', function (): void {
-    
+test('test [pl=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[pl=2]Padding left[/pl]'))->toEqual("  Padding left");
 });
 
-test('test [pr=] shortcodes', function (): void {
+test('test [pr=] shortcode', function (): void {
     expect(Termage::getShortcodes()->parse('[pr=2]Padding right[/pr]'))->toEqual("Padding right  ");
+});
+
+test('test [bg=] shortcode', function (): void {
+    expect(Termage::getShortcodes()->parse('[bg=#f00]BG[/bg]'))->toEqual("\e[48;2;255;0;0mBG\e[49m");
+});
+
+test('test [color=] shortcode', function (): void {
+    expect(Termage::getShortcodes()->parse('[color=#f00]COLOR[/color]'))->toEqual("\e[38;2;255;0;0mCOLOR\e[39m");
 });
 
 class ShortcodeTestTheme extends Theme implements ThemeInterface
