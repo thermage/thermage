49b5260ec8844a942e3acad4bb086aab125a3735
Awilum <sergey.romanenko@flextype.org>
2021-10-26 23:35:58 +0300

    refactor(termage): finetune code

--- src/Termage.php
+++ src/Termage.php
@@ -6,26 +6,22 @@ declare(strict_types=1);
  * Termage - Totally RAD Terminal styling for PHP! (https://digital.flextype.org/termage/)
  * Copyright (c) Sergey Romanenko (https://awilum.github.io)
  *
- * Licensed under The MIT License
+ * Licensed under The MIT License.
+ *
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
- *
- * @author    Sergey Romanenko <sergey.romanenko@flextype.org>
- * @copyright Copyright (c) Sergey Romanenko (https://awilum.github.io)
- * @link      https://digital.flextype.org/termage/ Termage
- * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace Termage;
 
 use Atomastic\Macroable\Macroable;
 use Termage\Elements\Alert;
-use Termage\Elements\Heading;
 use Termage\Elements\Anchor;
 use Termage\Elements\Bold;
 use Termage\Elements\Breakline;
 use Termage\Elements\Chart;
 use Termage\Elements\Div;
+use Termage\Elements\Heading;
 use Termage\Elements\Hr;
 use Termage\Elements\Italic;
 use Termage\Elements\Paragraph;
