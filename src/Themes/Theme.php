7adfe370ab4b67c2a14c6c70e342ae3eda6323a6
Awilum <sergey.romanenko@flextype.org>
2021-10-26 23:34:16 +0300

    refactor(theme): finetune code

--- src/Themes/Theme.php
+++ src/Themes/Theme.php
@@ -6,14 +6,10 @@ declare(strict_types=1);
  * Termage - Totally RAD Terminal styling for PHP! (https://digital.flextype.org/termage/)
  * Copyright (c) Sergey Romanenko (https://awilum.github.io)
  *
- * Licensed under The MIT License
+ * Licensed under The MIT License.
+ *
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
- *
- * @author    Sergey Romanenko <sergey.romanenko@flextype.org>
- * @copyright Copyright (c) Sergey Romanenko (https://awilum.github.io)
- * @link      https://digital.flextype.org/termage/ Termage
- * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace Termage\Themes;
@@ -227,9 +223,7 @@ class Theme implements ThemeInterface
             ],
 
             // Hr element.
-            'hr' => [
-                'text-align' => 'left',
-            ],
+            'hr' => ['text-align' => 'left'],
         ]);
     }
 
