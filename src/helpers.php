1b3a49e432e6f9d9f242bc7a1b43e4e331df90a4
Awilum <sergey.romanenko@flextype.org>
2021-10-26 11:34:46 +0300

    feat(helpers): rename `class` to `classes`

--- src/helpers.php
+++ src/helpers.php
@@ -57,69 +57,69 @@ function getTheme()
     return Termage::getTheme();
 }
 
-function div(string $value = '', string $class = ''): Div
+function div(string $value = '', string $classes = ''): Div
 {
-    return Termage::div($value, $class);
+    return Termage::div($value, $classes);
 }
 
-function paragraph(string $value = '', string $class = ''): Paragraph
+function paragraph(string $value = '', string $classes = ''): Paragraph
 {
-    return Termage::paragraph($value, $class);
+    return Termage::paragraph($value, $classes);
 }
 
-function span(string $value = '', string $class = ''): Span
+function span(string $value = '', string $classes = ''): Span
 {
-    return Termage::span($value, $class);
+    return Termage::span($value, $classes);
 }
 
-function hr(string $value = '', string $class = ''): Hr
+function hr(string $value = '', string $classes = ''): Hr
 {
-    return Termage::hr($value, $class);
+    return Termage::hr($value, $classes);
 }
 
-function anchor(string $value = '', string $class = ''): Anchor
+function anchor(string $value = '', string $classes = ''): Anchor
 {
-    return Termage::anchor($value, $class);
+    return Termage::anchor($value, $classes);
 }
 
-function bold(string $value = '', string $class = ''): Bold
+function bold(string $value = '', string $classes = ''): Bold
 {
-    return Termage::bold($value, $class);
+    return Termage::bold($value, $classes);
 }
 
-function italic(string $value = '', string $class = ''): Italic
+function italic(string $value = '', string $classes = ''): Italic
 {
-    return Termage::italic($value, $class);
+    return Termage::italic($value, $classes);
 }
 
-function underline(string $value = '', string $class = ''): Underline
+function underline(string $value = '', string $classes = ''): Underline
 {
-    return Termage::underline($value, $class);
+    return Termage::underline($value, $classes);
 }
 
-function strikethrough(string $value = '', string $class = ''): Strikethrough
+function strikethrough(string $value = '', string $classes = ''): Strikethrough
 {
-    return Termage::strikethrough($value, $class);
+    return Termage::strikethrough($value, $classes);
 }
 
-function breakline(string $value = '', string $class = ''): Breakline
+function breakline(string $value = '', string $classes = ''): Breakline
 {
-    return Termage::breakline($value, $class);
+    return Termage::breakline($value, $classes);
 }
 
-function chart(string $value = '', string $class = ''): Chart
+function chart(string $value = '', string $classes = ''): Chart
 {
-    return Termage::chart($value, $class);
+    return Termage::chart($value, $classes);
 }
 
-function alert(string $value = '', string $class = ''): Alert
+function alert(string $value = '', string $classes = ''): Alert
 {
-    return Termage::alert($value, $class);
+    return Termage::alert($value, $classes);
 }
 
-function heading(string $value = '', string $class = ''): Heading
+function heading(string $value = '', string $classes = ''): Heading
 {
-    return Termage::heading($value, $class);
+    return Termage::heading($value, $classes);
 }
 
 function color(): Color 
