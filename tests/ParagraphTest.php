74ef6279c2b7cd9e4ef76dc6a9cc4c33e4dd05c8
Awilum <sergey.romanenko@flextype.org>
2021-10-31 10:03:56 +0300

    feat(tests): update tests for `Paragraph` element

--- tests/ParagraphTest.php
+++ tests/ParagraphTest.php
@@ -6,6 +6,7 @@ use Termage\Termage;
 use function Termage\paragraph;
 
 test('test paragraph', function (): void {
+    putenv('COLUMNS=20');
     $value = paragraph('RAD')->render();
-    expect($value)->toBe("RAD" . PHP_EOL);
+    expect($value)->toBe("RAD                 \n");
 });
