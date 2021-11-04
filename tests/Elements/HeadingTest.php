87a4f575f1ec9fab75208b527c244ecc9ba70ae7
Awilum <sergey.romanenko@flextype.org>
2021-11-04 19:12:18 +0300

    feat(tests): add tests for `Heading` element

---
+++ tests/Elements/HeadingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Termage\Termage;
+use function Termage\heading;
+
+beforeEach(function() {
+    putenv('COLUMNS=20');
+});
+
+test('test heading size 1', function (): void {
+    $value = heading('RAD')->size1()->render();
+    expect(str_replace([PHP_EOL, " "], "", $value))->toBe("\e[1m╔══════════════════╗║RAD║╚══════════════════╝\e[22m");
+});
+
+test('test heading size 2', function (): void {
+    $value = heading('RAD')->size2()->render();
+    expect(str_replace([PHP_EOL, " "], "", $value))->toBe("\e[1m┌──────────────────┐│RAD│└──────────────────┘\e[22m");
+});
+
+test('test heading size 3', function (): void {
+    $value = heading('RAD')->size3()->render();
+    expect(str_replace([PHP_EOL, " "], "", $value))->toBe("\e[1mRAD\e[22m");
+});
+
+test('test heading size 4', function (): void {
+    $value = heading('RAD')->size4()->render();
+    expect(str_replace([PHP_EOL, " "], "", $value))->toBe("\e[1mRAD\e[22m");
+});
+
+test('test heading size 5', function (): void {
+    $value = heading('RAD')->size5()->render();
+    expect(str_replace([PHP_EOL, " "], "", $value))->toBe("\e[2mRAD\e[22m");
+});
