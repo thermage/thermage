53118393107e338bd78b30af6d7d1a9e560dee93
Awilum <sergey.romanenko@flextype.org>
2021-11-16 01:44:22 +0300

    refactor(styles): finetune

--- src/Base/Styles.php
+++ src/Base/Styles.php
@@ -16,209 +16,209 @@ namespace Termage\Base;
 
 use function Termage\getCsi;
 
-final class Styles {
-
+final class Styles
+{
     /**
      * Reset all styles and colors.
-     * 
+     *
      * @return string Result all styles and colors.
-     * 
+     *
      * @access public
      */
     public static function resetAll(): string
     {
-        return getCsi() . "0m";
+        return getCsi() . '0m';
     }
 
     /**
      * Set bold style.
-     * 
+     *
      * @return string Set bold style.
-     * 
+     *
      * @access public
      */
     public static function setBold(): string
     {
-        return getCsi() . "1m";
+        return getCsi() . '1m';
     }
 
     /**
      * Reset bold style.
-     * 
+     *
      * @return string Reset bold style.
-     * 
+     *
      * @access public
      */
-    public static function resetBold(): string 
+    public static function resetBold(): string
     {
-        return getCsi() . "22m";
+        return getCsi() . '22m';
     }
 
     /**
      * Set italic style.
-     * 
+     *
      * @return string Set italic style.
-     * 
+     *
      * @access public
      */
     public static function setItalic(): string
     {
-        return getCsi() . "3m";
+        return getCsi() . '3m';
     }
 
     /**
      * Reset italic style.
-     * 
+     *
      * @return string Reset italic style.
-     * 
+     *
      * @access public
      */
-    public static function resetItalic(): string 
+    public static function resetItalic(): string
     {
-        return getCsi() . "23m";
+        return getCsi() . '23m';
     }
 
     /**
      * Set underline style.
-     * 
+     *
      * @return string Set underline style.
-     * 
+     *
      * @access public
      */
     public static function setUnderline(): string
     {
-        return getCsi() . "4m";
+        return getCsi() . '4m';
     }
 
     /**
      * Reset underline style.
-     * 
+     *
      * @return string Reset underline style.
-     * 
+     *
      * @access public
      */
-    public static function resetUnderline(): string 
+    public static function resetUnderline(): string
     {
-        return getCsi() . "24m";
+        return getCsi() . '24m';
     }
 
     /**
      * Set strikethrough style.
-     * 
+     *
      * @return string Set strikethrough style.
-     * 
+     *
      * @access public
      */
     public static function setStrikethrough(): string
     {
-        return getCsi() . "9m";
+        return getCsi() . '9m';
     }
 
     /**
      * Reset strikethrough style.
-     * 
+     *
      * @return string Reset strikethrough style.
-     * 
+     *
      * @access public
      */
-    public static function resetStrikethrough(): string 
+    public static function resetStrikethrough(): string
     {
-        return getCsi() . "29m";
+        return getCsi() . '29m';
     }
 
     /**
      * Set dim style.
-     * 
+     *
      * @return string Set dim style.
-     * 
+     *
      * @access public
      */
     public static function setDim(): string
     {
-        return getCsi() . "2m";
+        return getCsi() . '2m';
     }
 
     /**
      * Reset dim style.
-     * 
+     *
      * @return string Reset dim style.
-     * 
+     *
      * @access public
      */
-    public static function resetDim(): string 
+    public static function resetDim(): string
     {
-        return getCsi() . "22m";
+        return getCsi() . '22m';
     }
 
     /**
      * Set blink style.
-     * 
+     *
      * @return string Set blink style.
-     * 
+     *
      * @access public
      */
     public static function setBlink(): string
     {
-        return getCsi() . "5m";
+        return getCsi() . '5m';
     }
 
     /**
      * Reset blink style.
-     * 
+     *
      * @return string Reset blink style.
-     * 
+     *
      * @access public
      */
-    public static function resetBlink(): string 
+    public static function resetBlink(): string
     {
-        return getCsi() . "25m";
+        return getCsi() . '25m';
     }
 
     /**
      * Set reverse style.
-     * 
+     *
      * @return string Set reverse style.
-     * 
+     *
      * @access public
      */
     public static function setReverse(): string
     {
-        return getCsi() . "7m";
+        return getCsi() . '7m';
     }
 
     /**
      * Reset reverse style.
-     * 
+     *
      * @return string Reset reverse style.
-     * 
+     *
      * @access public
      */
-    public static function resetReverse(): string 
+    public static function resetReverse(): string
     {
-        return getCsi() . "27m";
+        return getCsi() . '27m';
     }
 
     /**
      * Set invisible style.
-     * 
+     *
      * @return string Set invisible style.
-     * 
+     *
      * @access public
      */
     public static function setInvisible(): string
     {
-        return getCsi() . "8m";
+        return getCsi() . '8m';
     }
 
     /**
      * Reset invisible style.
-     * 
+     *
      * @return string Reset invisible style.
-     * 
+     *
      * @access public
      */
-    public static function resetInvisible(): string 
+    public static function resetInvisible(): string
     {
-        return getCsi() . "28m";
+        return getCsi() . '28m';
     }
 }
