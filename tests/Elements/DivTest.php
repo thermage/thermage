14721cb1944ea83e5fb9858661368252df7f8423
Awilum <sergey.romanenko@flextype.org>
2021-12-22 19:13:05 +0300

    fix(tests): update tests

--- tests/Elements/DivTest.php
+++ tests/Elements/DivTest.php
@@ -370,7 +370,7 @@ test('test textOverflow', function (): void {
     expect($element)->toBe(getCsi() . "0mThermage - Totall..." . PHP_EOL);
 
     $element = div()->value('Thermage - Totally RAD Terminal styling for PHP!')->textOverflowClip()->render();
-    expect($element)->toBe(getCsi() . "0mThermage - Totally   " . PHP_EOL . getCsi() . "0mRAD Terminal styling" . PHP_EOL . getCsi() . "0mfor PHP!            " . PHP_EOL);
+    expect($element)->toBe(getCsi() . "0mThermage - Totally  " . PHP_EOL . getCsi() . "0mRAD Terminal styling" . PHP_EOL . getCsi() . "0mfor PHP!            " . PHP_EOL);
 });
 
 class DivTestTheme extends Theme implements ThemeInterface
